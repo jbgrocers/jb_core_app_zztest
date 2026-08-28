f820582d65bfd0b58831862f06f82f4aef9e2d41
sachiingoel <sachingoel24@gmail.com>
2026-08-28 23:51:23 +0530

    Add initial app metadata to hooks.py

---
+++ jb_core_app_zztest/jb_core_app_zztest/hooks.py
@@ -0,0 +1,8 @@
+from . import __version__ as app_version
+
+app_name = "jb_core_app_zztest"
+app_title = "JB Core App ZZTest"
+app_publisher = "JB Grocers LLP"
+app_description = "Test app for Frappe Cloud validation cache diagnosis"
+app_email = "sachingoel24@gmail.com"
+app_license = "MIT"
